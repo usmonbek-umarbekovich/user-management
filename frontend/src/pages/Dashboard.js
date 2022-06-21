862aed27d3ee787eeb3f73599651d37825b77dc2
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-06-21 02:58:50 +0200

    implemented dashboard, showing users' data

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,132 @@
+import { useState, useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { useUserInfo } from '../contexts/userInfoContext';
+import adminService from '../features/adminService';
+import Table from 'react-bootstrap/Table';
+import Form from 'react-bootstrap/Form';
+import Stack from 'react-bootstrap/Stack';
+import Button from 'react-bootstrap/Button';
+import { FaUnlockAlt, FaTrashAlt } from 'react-icons/fa';
+
+function Dashboard() {
+  const [users, setUsers] = useState([]);
+  const [selectedUsers, setSelectedUsers] = useState([]);
+  const selectAllRef = useRef();
+
+  const { user: me, logoutUser } = useUserInfo();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (me == null) navigate('/login');
+    else {
+      adminService.getUsers().then(data => {
+        data.unshift({ ...me, selected: false });
+        data.forEach(u => (u.selected = false));
+        setUsers(data);
+      });
+    }
+  }, [me, navigate]);
+
+  const handleChange = (e, user) => {
+    if (user === null) {
+      users.forEach(u => {
+        u.selected = e.target.checked;
+      });
+
+      if (e.target.checked) setSelectedUsers(users);
+      else setSelectedUsers([]);
+      return;
+    }
+
+    user.selected = e.target.checked;
+    setSelectedUsers(prevUsers => {
+      if (user.selected) return [...prevUsers, user];
+
+      return prevUsers.filter(u => u._id !== user._id);
+    });
+  };
+
+  const handleStatus = status => {
+    adminService.changeStatus(status, selectedUsers).then(() => {
+      if (status === 'blocked' && selectedUsers.find(u => u._id === me._id)) {
+        logoutUser();
+        toast.info('You have been blocked');
+      }
+
+      selectedUsers.forEach(u => {
+        u.selected = false;
+        u.status = status;
+      });
+      selectAllRef.current.checked = false;
+      setSelectedUsers([]);
+    });
+  };
+
+  if (me == null) return null;
+
+  return (
+    <>
+      <Stack direction="horizontal" gap={4} className="align-items-center p-2">
+        <Button
+          variant="danger"
+          disabled={selectedUsers.length === 0}
+          onClick={() => handleStatus('blocked')}>
+          Block
+        </Button>
+        <Button
+          variant="outline-info"
+          disabled={selectedUsers.length === 0}
+          onClick={() => handleStatus('active')}>
+          <FaUnlockAlt />
+        </Button>
+        <Button variant="outline-danger" disabled={selectedUsers.length === 0}>
+          <FaTrashAlt />
+        </Button>
+      </Stack>
+      <Table bordered hover responsive variant="dark">
+        <thead>
+          <tr>
+            <th>
+              <Form.Check
+                ref={selectAllRef}
+                aria-label="select all the users"
+                onChange={e => handleChange(e, null)}
+              />
+            </th>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Last Login</th>
+            <th>Registration</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map(user => (
+            <tr
+              key={user._id}
+              className={`${user._id === me._id ? 'table-warning' : ''} ${
+                user.selected ? 'table-active' : ''
+              }`}>
+              <td>
+                <Form.Check
+                  aria-label={`select ${user.name}`}
+                  checked={user.selected}
+                  onChange={e => handleChange(e, user)}
+                />
+              </td>
+              <td>{user._id}</td>
+              <td>{user.name}</td>
+              <td>{user.email}</td>
+              <td>{user.lastLogin}</td>
+              <td>{user.registrationTime}</td>
+              <td>{user.status}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </>
+  );
+}
+export default Dashboard;
