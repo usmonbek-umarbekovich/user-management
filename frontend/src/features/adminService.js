b754668f1dbe9c6fc648c6cd01355ba8ebbf7b30
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-06-21 02:53:54 +0200

    getting user data, blocking/unbloking users

---
+++ frontend/src/features/adminService.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const API_URL = '/api/users';
+
+const getUsers = async () => {
+  const response = await axios.get(`${API_URL}`);
+  return response.data;
+};
+
+const changeStatus = async (status, selectedUsers) => {
+  const endpoint = status === 'blocked' ? 'block' : 'unblock';
+  await axios.put(`${API_URL}/${endpoint}`, { selectedUsers });
+};
+
+const deleteUsers = async () => {};
+
+const adminService = {
+  getUsers,
+  changeStatus,
+  deleteUsers,
+};
+
+export default adminService;
