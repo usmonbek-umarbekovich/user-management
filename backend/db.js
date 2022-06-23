3a5e694c9aba20502421dcd8e9c18226476c88b8
Usmonbek Rustamov <75022353+usmonbek-umarbekovich@users.noreply.github.com>
2022-06-23 20:59:59 +0200

    added mongodb session

--- backend/db.js
+++ backend/db.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`Database Connected: ${conn.connection.host}`);
+    const mongo = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`Database Connected: ${mongo.connection.host}`);
+    return mongo;
   } catch (error) {
     console.log(error);
     process.exit(1);
